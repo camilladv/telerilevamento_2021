1740cc16636f5520c0b0bb3e3f230494bb474215
camilladv <80789975+camilladv@users.noreply.github.com>
2021-04-08 18:26:14 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -4,3 +4,32 @@
 
 library(raster)
 setwd('C:/lab/greenland')
+
+#importiano una time series su R
+lst_2000 <- raster("lst_2000.tif")     #lst=land surface temperature, è la temperatura misurata al suolo da un satellite di Copernicus
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+# par
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# list f files:
+rlist <- list.files(pattern="lst")
+rlist
+
+#dare le informazini di tutte le 4 immagini in un unico comando
+import <- lapply(rlist,raster)
+import
+
+TGr <- stack(import)
+plot(TGr)
+
+plotRGB(TGr, 1, 2, 3, stretch="Lin") 
+plotRGB(TGr, 2, 3, 4, stretch="Lin") 
+plotRGB(TGr, 4, 3, 2, stretch="Lin") 
+
