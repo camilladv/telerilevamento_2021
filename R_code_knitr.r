7fab1561b9a2c186f6e9b7cc2218f2fd6d5c76ea
camilladv <80789975+camilladv@users.noreply.github.com>
2021-04-16 09:32:37 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,1 +1,10 @@
+#R_code_knitr.r
+#il pacchetto knitr è all'interno di R e utilizza un codice esterno per creare un reporp
+#prende un codice all'esterno, lo importa all'interno di R e qui genera un report che viene salvato nella stessa cartella in cui è presente il codice
+
+setwd('C:/lab')
+
+#install.packages('knitr')
+library(knitr)  #o require(knitr), è uguale
+
 
