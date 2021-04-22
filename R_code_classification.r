0a6c6eb9168435f4ab055c85bacab40ac9511d6b
camilladv <80789975+camilladv@users.noreply.github.com>
2021-04-22 11:40:49 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,1 +1,10 @@
+#R_code_classification.r
 
+library(raster)
+setwd('C:/lab/')
+
+#carichiamo l'immagine ESA che ha 3 livelli con la funzione brick. Prende i 3 livelli e li unisce
+so<-brick('Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg')
+so
+
+#
