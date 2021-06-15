f76001806eaaf93a423d6dad46aeae48cb9cb50a
camilladv <80789975+camilladv@users.noreply.github.com>
2021-06-15 22:21:48 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -3,9 +3,16 @@ library(raster)
 #set working directory
 setwd('C:/lab/Progetto')
 
-#importare l'immagine
-#1 livello
+#importo l'immagine del 05/2013 con tutte le sue bande
+lv13<-brick('lakevictoria_oli_2013141_lrg_8513.jpg')  #needs pacchetto raster
+#importo l'immagine del 05/2021 con tutte le sue bande
+lv21<-brick('lakevictoria_oli_2021147_lrg_27521.jpg')
 
-#tutte le bande
+#confronto a colori reali delle 2 immagini
+par(mfrow=c(2,1))
+plotRGB(lv13,r=1,g=2,b=3,stretch='lin')
+plotRGB(lv21,r=1,g=2,b=3,stretch='lin')
+
+
 
 
