2588bd34d73f957ced13ea3ec69eed4c8989f9af
camilladv <80789975+camilladv@users.noreply.github.com>
2021-06-30 17:38:08 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -1,19 +1,52 @@
+#install.packages("raster")
 library(raster)
 
 #set working directory
 setwd('C:/lab/Progetto')
 
-#importo l'immagine del 05/2013 con tutte le sue bande
-lv13<-brick('lakevictoria_oli_2013141_lrg_8513.jpg')  #needs pacchetto raster
-#importo l'immagine del 05/2021 con tutte le sue bande
-lv21<-brick('lakevictoria_oli_2021147_lrg_27521.jpg')
+#importo l'immagine del 1979 (prima dell'eruzione) con tutti i suoi livelli
+sth79<-brick('sthelens_ms3_19790829_lrg.jpg')  #serve il pacchetto raster
+#guardo le informazioni relative all'immagine
+sth79 #ha 3 bande: B1 NIR, B2 red e B3 green
+#visualizzo l'immagine che ha la banda del NIR sul rosso. Viene evidenziata la vegetazione
+plotRGB(sth79,stretch='lin')  #siccome le bande sono NIR sul red, red sul green e green sul blue non serve definirle esplicitamente
 
-#confronto a colori reali delle 2 immagini
-par(mfrow=c(2,1))
-plotRGB(lv13,r=1,g=2,b=3,stretch='lin')
-plotRGB(lv21,r=1,g=2,b=3,stretch='lin')
+#importo l'immagine del 1980 (dopo l'eruzione) con tutte le sue bande
+sth80<-brick('sthelens_ms2_19800924_lrg.jpg')
+plotRGB(sth80,stretch='lin')
 
-#NON VA BENE, PERCHè LE BANDE UTILIZZATE SONO A COLORI VISIBILI
+#plotto le due immagini in contemporanea
+par(mfrow=c(1,2)) #1 colonna, 2 righe
+plotRGB(sth79,stretch='lin')   
+plotRGB(sth80,stretch='lin')  #si può notare come è cambiato il colore dell'acqua nel lago sotto, vicino al cratere. Il blu è più chiaro, indica maggiori solidi al suo interno
+
+#calcolo NDVI per sth79, quindi cerco il nome delle bande
+# names      : sthelens_ms3_19790829_lrg.1, sthelens_ms3_19790829_lrg.2, sthelens_ms3_19790829_lrg.3 
+#rinomino le bande del NIR e del red
+NIR79<-sth79$sthelens_ms3_19790829_lrg.1
+RED79<-sth79$sthelens_ms3_19790829_lrg.2
+NDVI79<-(NIR79-RED79)/(NIR79+RED79)
+
+#calcolo NDVI per sth79, quindi cerco il nome delle bande
+# names      : sthelens_ms2_19800924_lrg.1, sthelens_ms2_19800924_lrg.2, sthelens_ms2_19800924_lrg.3 
+NIR80<-sth80$sthelens_ms2_19800924_lrg.1
+RED80<-sth80$sthelens_ms2_19800924_lrg.2
+NDVI80<-(NIR80-RED80)/(NIR80+RED80)
+
+#confronto i due valori di NDVI per il 1979, prima dell'eruzione e il NDVI per il 1970, dopo l'eruzione
+difNDVI<-NDVI79-NDVI80
+plot(difNDVI)
+
+
+
+
+
+#install.packages("RStoolbox")
+library(RStoolbox)  #per calcolare gli indici di vegetazione
+#vari indici di vegetazione per sth79
+vi79<-spectralIndices(sth79,green=3,red=2,nir=1)
+
+
 
 
 
