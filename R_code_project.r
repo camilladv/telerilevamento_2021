7b7d227bf7fbcfbf08dfeb8daf7d7a8e567dade6
camilladv <80789975+camilladv@users.noreply.github.com>
2021-07-03 19:05:28 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -1,5 +1,5 @@
 #install.packages('raster')
-#install.packages'RStoolbox')
+#install.packages('RStoolbox')
 #install.packages('ggplot2')
 #install.packages('gridExtra')
 library(raster)
@@ -13,7 +13,7 @@ setwd('C:/lab/Progetto')
 #importo l'immagine del 1979 (prima dell'eruzione) con tutti i suoi livelli
 sth79<-brick('sthelens_ms3_19790829_lrg.jpg')  #serve il pacchetto raster
 #guardo le informazioni relative all'immagine
-sth79 #ha 3 bande: B1 NIR, B2 red e B3 green
+#sth79 ha 3 bande: NIR, red e green
 #visualizzo l'immagine che ha la banda del NIR sul rosso. Viene evidenziata la vegetazione
 ggRGB(sth79,r=1,g=2,b=3,stretch='lin')  #coordinate non reali
 
@@ -28,6 +28,7 @@ grid.arrange(h79,h81,nrow=1)  #immagini disposte su 1 riga
 dev.off()
 
 #calcolo NDVI per sth79, quindi cerco il nome delle bande
+sth79
 # names      : sthelens_ms3_19790829_lrg.1, sthelens_ms3_19790829_lrg.2, sthelens_ms3_19790829_lrg.3 
 #rinomino le bande del NIR e del red
 NIR79<-sth79$sthelens_ms3_19790829_lrg.1  #con il $ lego la banda all'immagine
@@ -35,14 +36,15 @@ RED79<-sth79$sthelens_ms3_19790829_lrg.2
 NDVI79<-(NIR79-RED79)/(NIR79+RED79)
 
 #calcolo NDVI per sth81, quindi cerco il nome delle bande
+sth89
 # names      : sthelens_ms3_19810823_lrg.1, sthelens_ms3_19810823_lrg.2, sthelens_ms3_19810823_lrg.3  
 NIR81<-sth81$sthelens_ms3_19810823_lrg.1
 RED81<-sth81$sthelens_ms3_19810823_lrg.2
 NDVI81<-(NIR81-RED81)/(NIR81+RED81)
 
-#confronto i due valori di NDVI del 1979 (prima dell'eruzione) e l'NDVI del 1981 (dopo l'eruzione)
+#confronto graficamente i due valori di NDVI del 1981 (dopo l'eruzione) e del 1979 (prima dell'eruzione)
 difNDVI<-NDVI81-NDVI79
-cl<-colorRampPalette(c('blue','light green','red'))(100)   
+cl<-colorRampPalette(c('blue','light green','red'))(100)  #definisce i colori per il plot 
 plot(difNDVI,col=cl,main='NDVI difference')  #le zone con differenza di NDVI negativa, ovvero che hanno subito una maggiore perdita di vegetazione, sono in blu
 
 #Copertura del suolo prima e dopo l'eruzione. Classificazione in 5 classi
@@ -65,7 +67,7 @@ c5<-colorRampPalette(c('brown','blue','green','gray','light green'))(100)
 plot(sth79c$map,col=c5,main='1979 classification')
 #classe 1: roccia + suolo nudo, classe 2: acqua + alcuni versanti, classe 3: foresta, classe 4: neve + suolo nudo, classe 5: vegetazione
 #calcolo la percentuale di copertura delle varie classi
-freq(sth79c$map)
+freq(sth79c$map)  #numero di pixel per ciascuna classe
 #      value  count
 # [1,]     1 201241
 # [2,]     2 105470
@@ -74,7 +76,7 @@ freq(sth79c$map)
 # [5,]     5 376408
 s79<-1088336  #totale di pixel ricavato dalle informazioni dell'immagine
 prop79<-freq(sth79c$map)/s79
-prop79
+prop79  #percentuali di pixel per ciascuna classe
 #           value      count
 # [1,] 9.188339e-07 0.18490705   --> 18% roccia + suolo nudo
 # [2,] 1.837668e-06 0.09690941   --> 10% acqua + alcuni versanti
@@ -100,7 +102,7 @@ c5<-colorRampPalette(c('brown','blue','green','gray','light green'))(100)
 plot(sth81c$map,col=c5,main='1981 classification')
 #classe 1: roccia + suolo nudo, classe 2: acqua + alcuni versanti, classe 3: foresta, classe 4: neve + suolo nudo, classe 5: vegetazione
 #calcolo la percentuale di copertura delle varie classi
-freq(sth81c$map)
+freq(sth81c$map)  #numero di pixel per ciascuna classe
 #      value  count
 # [1,]     1 164132
 # [2,]     2 186974
@@ -109,7 +111,7 @@ freq(sth81c$map)
 # [5,]     5 297515
 s81<-1098097  #totale di pixel ricavato dalle informazioni dell'immagine
 prop81<-freq(sth81c$map)/s81
-prop81
+prop81  #percentuali di pixel per ciascuna classe
 #             value     count
 # [1,] 9.106664e-07 0.1494695   --> 15% roccia + suolo nudo
 # [2,] 1.821333e-06 0.1702709   --> 17% acqua + alcuni versanti
@@ -117,11 +119,12 @@ prop81
 # [4,] 3.642665e-06 0.0811850   --> 8% suolo nudo
 # [5,] 4.553332e-06 0.2709369   --> 27% vegetazione
 
-cover<-c('Roccia','Acqua','Foresta','Suolo nudo','Vegetazione')  #colonna cover contenente le 5 classi di copertura del suolo  
+#costruisco il datebase per osservare la differenza di copertura del suolo a seguito dell'eruzione
+cover<-c('Roccia','Acqua','Foresta','Suolo nudo','Vegetazione')  #colonna cover contenente le 5 classi di copertura del suolo. c unisce dei valori vettoriali
 perc_79<-c(18.49,9.69,33.00,4.22,34.58) #colonna contenente i valori percentuali di sth79c che ricavo da prop79
 perc_81<-c(14.94,17.02,32.81,8.11,27.09)  #colonna contenente i valori percentuali di sth81c che ricavo da prop81
 #creo il dataframe con il comando data.frame
-cover_perc<-data.frame(cover,perc_79,perc_81)
+cover_perc<-data.frame(cover,perc_79,perc_81) #indico le colonne che deve inserire nel dataframe
 cover_perc
 #         cover perc_79 perc_81
 # 1      Roccia   18.49   14.94
@@ -132,9 +135,11 @@ cover_perc
 
 #Firme spettrali delle zone che hanno subito una maggiore differrenziazione 
 
+#seleziono i punti dalle immagini
 plotRGB(sth79)  #apro il plot su cui cliccare
 click(sth79,id=T,xy=T,cell=T,type='p',pch=16,col='yellow')  #immagine, crea un id per ogni punto selezionato; utilizziamo un'informazione spaziale;
-                                                            #n. pixel cliccato; p point; forma simbolo; colore simbolo nella mappa
+                                                            #numero del pixel cliccato; tipo di selezione a punto; forma simbolo nella mappa; colore simbolo nella mappa
+#informazioni di riflettanza dei pixel selezionati
 #      x     y   cell    sthelens_ms3_19790829_lrg.1 sthelens_ms3_19790829_lrg.2 sthelens_ms3_19790829_lrg.3
 # 1 724.5 793.5 291885                           35                          22                          48
 #                        
@@ -157,7 +162,7 @@ click(sth81,id=T,xy=T,cell=T,type='p',pch=16,col='yellow')
 
 #definire le colonne del dataset
 band<-c(1,2,3) #sono le 3 bande dell'immagine
-sth79_1<-c(35,22,48) #valori di riflettanza nel punto 1 in sth79. Ogni colonna è un pixel. Punto 1: Spirit Lake (lago a Nord del cratere)
+sth79_1<-c(35,22,48) #valori di riflettanza nel punto 1 in sth79. Punto 1: Spirit Lake (lago a Nord del cratere)
 sth81_1<-c(190,172,152) #valori di riflettanza nel punto 1 in sth81
 sth79_2<-c(146,21,29) #Punto 2: Foresta
 sth81_2<-c(218,216,201)
